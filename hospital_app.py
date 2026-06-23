ee112af2c435adec1b27d0243c68f52d5a0f95d0
Reynard712 <reyfredrick123@gmail.com>
2026-06-23 14:59:19 +0700

    u

--- hospital_app.py
+++ hospital_app.py
@@ -327,7 +327,7 @@ if submitted:
 
         st.markdown(bars_html + """
             <!-- COLOR7 -->
-            <div style="margin-top:20px;background:#e90ee9;border:1px solid #ff0505;
+            <div style="margin-top:20px;background:#3fb9ff;border:1px solid #ff0505;
                         border-radius:10px;padding:12px 14px;font-size:12px;color:#1af250;">
                 <strong>Model:</strong>KNN (k=7) · 102,000 patients · 99.5% accuracy<br>
                 <strong>Powered by:</strong> Future Classroom ML
