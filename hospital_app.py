f9542091aba1b2aa6fc6766d2bad06724c07ba28
Reynard712 <reyfredrick123@gmail.com>
2026-06-23 15:03:44 +0700

    fgd

--- hospital_app.py
+++ hospital_app.py
@@ -206,12 +206,12 @@ with st.form("triage_form"):
     # Section 5 — Patient Info
     st.markdown("""
     <!-- COLOR6 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#09e2f6;border:1px solid #00c8ff;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:#f2a90a;color:rgb(0, 0, 0);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">5</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">Patient Information</span>
+            <span style="font-size:16px;font-weight:600;color:#000000;">Patient Information</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
